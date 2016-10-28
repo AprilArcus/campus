7981e6f101ef41a72e63f0f2ed0e95637e9061d3
April Arcus <april.arcus@gmail.com>
2016-10-27 23:41:50 -0700

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: __dirname + '/build/client',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello from Webpack!'
+    })
+  ]
+}
